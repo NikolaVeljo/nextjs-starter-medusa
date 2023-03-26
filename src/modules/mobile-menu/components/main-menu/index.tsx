6da9f33ba5899c17a5b5e43267697e07279183d6
Nikola Veljovic <nikola.veljovic@UC-C02SV9JHGTFL.local>
2023-03-26 11:31:47 +0200

    Changes mobile navigation & adds black logo

--- src/modules/mobile-menu/components/main-menu/index.tsx
+++ src/modules/mobile-menu/components/main-menu/index.tsx
@@ -7,6 +7,7 @@ import X from "@modules/common/icons/x"
 import { useCollections, useMeCustomer } from "medusa-react"
 import Link from "next/link"
 import ReactCountryFlag from "react-country-flag"
+import Image from "next/image"
 
 const MainMenu = () => {
   const { collections } = useCollections()
@@ -25,18 +26,19 @@ const MainMenu = () => {
 
   return (
     <div className="flex flex-col flex-1">
-      <div className="flex items-center justify-between w-full border-b border-gray-200 py-4 px-6">
+      <div className="flex items-center justify-between w-full border-b border-gray-200 py-2 px-6">
         <div className="flex-1 basis-0">
-          <button
-            className="flex items-center gap-x-2"
-            onClick={setScreenCountry}
-          >
-            <ReactCountryFlag countryCode={countryCode || "us"} svg />
-            <ChevronDown />
-          </button>
-        </div>
-        <div>
-          <h1 className="text-xl-semi uppercase">Acme</h1>
+            <Image
+              src="/FelixVita-logo-black.png"
+              width={50}
+              height={50}
+              loading="eager"
+              priority={true}
+              quality={100}
+              objectFit="scale-down"
+              alt="Logo"
+              draggable="false"
+            />
         </div>
         <div className="flex-1 basis-0 flex justify-end">
           <button onClick={close}>
@@ -134,25 +136,6 @@ const MainMenu = () => {
                 </Link>
               </div>
             )}
-            <div className="flex flex-col gap-y-4">
-              <span className="text-gray-700 uppercase">Delivery</span>
-              <button
-                className="flex items-center justify-between border-b border-gray-200 py-2"
-                onClick={setScreenCountry}
-              >
-                <span className="sr-only">
-                  Click to select shipping country
-                </span>
-                <div className="flex items-center gap-x-2">
-                  <ReactCountryFlag countryCode={countryCode || "us"} svg />
-                  <span className="normal-case">
-                    Shipping to{" "}
-                    {countries?.find((c) => c.country === countryCode)?.label}
-                  </span>
-                </div>
-                <ChevronDown className="-rotate-90" />
-              </button>
-            </div>
           </div>
         </div>
       </div>
