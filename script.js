c5da513a86cac8315209de9d5bbbbe2b31ab8ebd
Nikola Veljovic <nikola.veljovic@UC-C02SV9JHGTFL.local>
2023-08-31 08:42:21 +0200

    Felix vita becomes felix vita simple

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('menuButton').addEventListener('click', function() {
+  document.getElementById('constructionText').style.opacity = '0';
+  this.style.opacity = '0';
+  document.getElementById('menuOverlay').style.display = 'block';
+});
